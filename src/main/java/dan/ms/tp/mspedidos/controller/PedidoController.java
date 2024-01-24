4dad46bd0955a67edb56463a45702237b918de16
elPeiretti <tomaspeiretti@gmail.com>
2024-01-24 10:20:00 -0300

    add TODO controller handlers in PedidoController

--- src/main/java/dan/ms/tp/mspedidos/controller/PedidoController.java
+++ src/main/java/dan/ms/tp/mspedidos/controller/PedidoController.java
@@ -5,13 +5,17 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import dan.ms.tp.mspedidos.modelo.Pedido;
 import dan.ms.tp.mspedidos.service.PedidoService;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 @RestController
 @RequestMapping("api/pedido")
@@ -23,11 +27,36 @@ public class PedidoController {
 
     @PostMapping
     public ResponseEntity<Pedido> guardar(@RequestBody Pedido pedido){
+        //TODO
         return ResponseEntity.ok().build();
     }
 
+    /*
+     * Busqueda por cliente y/o fecha
+     */
     @GetMapping
-    public ResponseEntity<List<Pedido>> buscar(){
+    public ResponseEntity<List<Pedido>> buscar(@RequestParam String idCliente, @RequestParam(required = false) String fecha){
+        //TODO
         return ResponseEntity.ok().build();
     }
+
+    /*
+     * Busqueda por id
+     */
+    @GetMapping("/{id}")
+    public ResponseEntity<List<Pedido>> buscarPorId(@PathVariable String id){
+        //TODO
+        return ResponseEntity.ok().build();
+    }
+
+    /*
+     * Actualizacion de estado de pedido a CANCELADO
+     */
+    @PutMapping()
+    public ResponseEntity<Pedido> cancelar(@RequestBody Pedido entity) {
+        //TODO
+        return ResponseEntity.ok().build();
+    }
+
+
 }
