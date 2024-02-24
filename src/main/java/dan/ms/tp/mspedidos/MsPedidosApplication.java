998e2120348d4dc5c64a5f570a70ed79c5a74d2b
ChortJulio <chortjulio@gmail.com>
2024-02-24 19:10:18 -0300

    Add CORS policy allowing all domains

--- src/main/java/dan/ms/tp/mspedidos/MsPedidosApplication.java
+++ src/main/java/dan/ms/tp/mspedidos/MsPedidosApplication.java
@@ -2,12 +2,28 @@ package dan.ms.tp.mspedidos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MsPedidosApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MsPedidosApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(MsPedidosApplication.class, args);
+  }
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+            .allowedOrigins("*")
+            .allowedHeaders("*");
+      }
+    };
+  }
 
 }
