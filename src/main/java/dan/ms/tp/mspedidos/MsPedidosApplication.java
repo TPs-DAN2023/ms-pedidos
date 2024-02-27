de4b5f55de97f73e3a9216596655348be361ba2a
ChortJulio <chortjulio@gmail.com>
2024-02-27 10:49:49 -0300

    Remove CORS policy (hate you CORS!)

--- src/main/java/dan/ms/tp/mspedidos/MsPedidosApplication.java
+++ src/main/java/dan/ms/tp/mspedidos/MsPedidosApplication.java
@@ -2,9 +2,6 @@ package dan.ms.tp.mspedidos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MsPedidosApplication {
@@ -12,18 +9,4 @@ public class MsPedidosApplication {
   public static void main(String[] args) {
     SpringApplication.run(MsPedidosApplication.class, args);
   }
-
-  @Bean
-  public WebMvcConfigurer corsConfigurer() {
-    return new WebMvcConfigurer() {
-      @Override
-      public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
-            .allowedOrigins("*")
-            .allowedHeaders("*");
-      }
-    };
-  }
-
 }
